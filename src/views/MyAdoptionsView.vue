6ddf6e2218514ae84211f7b0ef8155894516430c
Akamnex666 <eliisaruiz775@gmail.com>
2025-08-08 16:06:22 -0500

    Add MyAdoptionsView and MyAdoptionsList components; update AdoptionsView with new icons and adjust CSS variables

---
+++ src/views/MyAdoptionsView.vue
@@ -0,0 +1,10 @@
+
+<template>
+  <HeaderWithProfile :show-back-to-forum="true" />
+  <MyAdoptionsList />
+</template>
+
+<script setup>
+import MyAdoptionsList from '@/components/MyAdoptionsList.vue'
+import HeaderWithProfile from '@/components/HeaderWithProfile.vue'
+</script>
